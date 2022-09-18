163603a0faa79e21ff245ca25b929f748f3f3c18
geongu kang <kdr06006@naver.com>
2022-09-18 19:45:59 +0900

    add two buttons

--- app/src/main/java/com/rumble/MainActivity.java
+++ app/src/main/java/com/rumble/MainActivity.java
@@ -2,7 +2,10 @@ package com.rumble;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonFall = (Button)findViewById(R.id.buttonFall);
+        Button buttonSTT = (Button)findViewById(R.id.buttonSTT);
+
+        buttonFall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), FallActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonSTT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), STTActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
