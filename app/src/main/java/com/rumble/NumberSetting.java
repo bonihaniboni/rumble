7713b4284c6e03108ed329708b1d029f61eb6d9b
geongu kang <kdr06006@naver.com>
2022-09-18 20:03:50 +0900

    add NumberActivity

---
+++ app/src/main/java/com/rumble/NumberSetting.java
@@ -0,0 +1,14 @@
+package com.rumble;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NumberSetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_number_setting);
+    }
+}
